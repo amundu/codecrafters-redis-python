f844f0c8c2c5d63aadceaaf50a7e7fc8ba54b487
amundu <alexismun53@gmail.com>
2024-11-25 09:47:04 -0600

    respond to multiple PING commands

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ import socket  # noqa: F401
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept() # wait for client
-    connection.sendall(b"+PONG\r\n")
+    decoded_data = connection.recv(1024).decode("utf-8").split("\n")
+    for command in decoded_data:
+        if command == "PING":
+            connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
