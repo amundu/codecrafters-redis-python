8a7d7ad0021c8ed30451420916d9af81e172b18f
amundu <alexismun53@gmail.com>
2024-11-25 10:46:00 -0600

    add loop to handle multiple pings

--- app/main.py
+++ app/main.py
@@ -4,10 +4,15 @@ import socket  # noqa: F401
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept() # wait for client
-    decoded_data = connection.recv(1024).decode("utf-8").split("\n")
-    for command in decoded_data:
-        if command == "PING":
-            connection.sendall(b"+PONG\r\n")
+    print("Accepted connection!")
+    try:
+        while True:
+            connection.recv(1024)
+            connection.sendall(b"+PONG\r\n") 
+    except:
+        pass
+    finally:
+        connection.close()
 
 
 if __name__ == "__main__":
